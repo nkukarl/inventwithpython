8d5ad77132a4dc4e8fee7d9d74f3b40d0fcde8d2
nkukarl <nkukarl@users.noreply.github.com>
2014-10-06 14:23:19 +1100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -50,11 +50,6 @@ hangmanpics = ['''
       |
 =========''']
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
-
-word = words[random.randrange(0, len(words))]
-
-print(word)
-
 def add_underline(string):
   new_string = ""
   for i in string:
@@ -62,36 +57,68 @@ def add_underline(string):
     new_string += " "
   return new_string
 
-missed_letters = ""
-guess_result = "_" * len(word)
+def check_input(string):
+    global guessed_letters
+    if len(string) != 1:
+        print("*ONLY ONE* letter!")        
+        return True
+    elif not ('a' <= string.lower() <= 'z'):
+        print("a *LETTER*!")
+        return True
+    elif string in guessed_letters:
+        print("Already guessed!")
+        return True
+    else:
+        guessed_letters += string.lower()
+        return False
 
-cnt = 0
+to_play = True
 
-print hangmanpics[cnt]
-print add_underline(guess_result)
-print "missed letter: ", missed_letters
-
-while cnt < 6:
-  print("**********************************")
-  # guessed_letter = input("Guess a letter: ")
-  guessed_letter = chr(random.randrange(97, 123))
-  print guessed_letter
-  new_guess_result = ""
-  if guessed_letter in word:
-    for i in range(0, len(word)):
-      if word[i] == guessed_letter:
-        new_guess_result += guessed_letter
+while to_play:
+    word = words[random.randrange(0, len(words))]
+    
+    guessed_letters = ""
+    
+    print(word)
+    
+    missed_letters = ""
+    guess_result = "_" * len(word)
+    
+    cnt = 0
+    
+    print(hangmanpics[cnt])
+    print("Guess a", len(word), "letter word.")
+    print(add_underline(guess_result))
+    print("missed letter: ", missed_letters)
+    
+    while cnt < 6:
+      print("**********************************")
+      input_flag = True
+      while input_flag:
+          guessed_letter_temp = input("Guess a letter: ")
+          input_flag = check_input(guessed_letter_temp)
+      # guessed_letter = chr(random.randrange(97, 123))
+      guessed_letter = guessed_letter_temp.lower()
+      new_guess_result = ""
+      if guessed_letter in word:
+        for i in range(0, len(word)):
+          if word[i] == guessed_letter:
+            new_guess_result += guessed_letter
+          else:
+            new_guess_result += guess_result[i]
+        guess_result = new_guess_result
       else:
-        new_guess_result += guess_result[i]
-    guess_result = new_guess_result
-  else:
-    missed_letters += guessed_letter
-    cnt += 1
-  print hangmanpics[cnt]
-  print add_underline(guess_result)
-  print "missed letter: ", missed_letters
-  if guess_result == word:
-      print("You win!")
-      break
-else:
-  print("You lose!")
+        missed_letters += guessed_letter
+        cnt += 1
+      print(hangmanpics[cnt])
+      print(add_underline(guess_result))
+      print("missed letter: ", missed_letters)
+      if guess_result == word:
+          print("You win!")
+          break
+    else:
+      print("You lose!")
+    
+    user_choice = input("Continue to play? (y/n) ")
+    if user_choice == "n":
+        to_play = False
